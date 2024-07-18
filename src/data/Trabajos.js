530dc24e3452de8c561d0c971f57b01ea46bcb60
Tatiana Hernández <tatianahernandez@MacBook-Air-de-Tatiana.local>
2024-07-18 16:06:41 +0200

    ajust projects

--- src/data/Trabajos.js
+++ src/data/Trabajos.js
@@ -1,68 +1,76 @@
 export const trabajos = [
   {
-    'id': 'Pharma',
-    'nombre': 'Pharma.',
-    'url': 'https://tati20h.github.io/pharma/#/home',
-    'tecnologias': 'REACT.JS,MUIX, OpenFDA PI',
-    'categorias': 'Full-Stack',
-    'imagen': 'https://github.com/Tati20h/pharma/blob/main/src/assets/img/logo.png?raw=true',
-    'Descripcion': 'Web in React that uses the OpenFDA API that allows searching for US medications'
+    id: "Pharma",
+    nombre: "Pharma.",
+    url: "https://tati20h.github.io/pharma/#/home",
+    tecnologias: "REACT.JS, MUIX, OpenFDA API",
+    categorias: "Full-Stack",
+    imagen:
+      "https://github.com/Tati20h/pharma/blob/main/src/assets/img/logo.png?raw=true",
+    Descripcion:
+      "Web in React that uses the OpenFDA API that allows searching for US medications",
   },
 
   {
-    'id': 'frontend-ehour',
-    'nombre': 'Labor certificates.',
-    'url': 'https://certifications-generator-jmbrv.ondigitalocean.app/',
-    'tecnologias': 'HTML, CSS, JS, REACT, FIGMA,MUIX',
-    'categorias': 'Front-end',
-    'imagen': 'https://github.com/Tati20h/08-portafolio/blob/main/src/assets/img/certipdf.png?raw=true',
-    'Descripcion': 'Generator of labor certificates in PDF format.'
+    id: "frontend-ehour",
+    nombre: "Labor certificates.",
+    url: "https://certifications-generator-jmbrv.ondigitalocean.app/",
+    tecnologias: "REACT.JS, NODEJS - EXPRESS, FIGMA, MUIX",
+    categorias: "Front-end",
+    imagen:
+      "https://github.com/Tati20h/08-portafolio/blob/main/src/assets/img/certipdf.png?raw=true",
+    Descripcion: "Generator of labor certificates in PDF format.",
   },
 
   {
-    'id': 'data-lovers',
-    'nombre': 'Delie Taste.',
-    'url': 'https://yennyferjp.github.io/DEV008-data-lovers/',
-    'tecnologias': 'HTML, CSS, JS,(Git y GitHub), FIGMA, Web APIs',
-    'categorias': 'Front-end',
-    'imagen': 'https://github.com/Tati20h/DEV008-data-lovers/blob/main/src/img/imgbanner.png?raw=true',
-    'Descripcion': 'E-commerce restaurant website',
+    id: "data-lovers",
+    nombre: "Delie Taste.",
+    url: "https://yennyferjp.github.io/DEV008-data-lovers/",
+    tecnologias: "HTML, CSS, JAVASCRIPT, FIGMA, Web APIs",
+    categorias: "Front-end",
+    imagen:
+      "https://github.com/Tati20h/DEV008-data-lovers/blob/main/src/img/imgbanner.png?raw=true",
+    Descripcion: "E-commerce restaurant website",
   },
   {
-    'id': 'social-network',
-    'nombre': 'Warmi.',
-    'url': 'https://tati20h.github.io/DEV008-social-network/',
-    'tecnologias': 'HTML, CSS, JS, SPA, Git, GitHub, FIGMA, Design Responsive, Firestore,  Firebase ',
-    'imagen': 'https://github.com/Tati20h/08-portafolio/blob/main/src/assets/img/warmi.png?raw=true',
-    'categorias': 'Front-end',
-    'Descripcion': 'Social Network for Latina Women Abroad'
+    id: "social-network",
+    nombre: "Warmi.",
+    url: "https://tati20h.github.io/DEV008-social-network/",
+    tecnologias:
+      "HTML, CSS, JS, SPA, FIGMA, Design Responsive, Firestore, Firebase",
+    imagen:
+      "https://github.com/Tati20h/08-portafolio/blob/main/src/assets/img/warmi.png?raw=true",
+    categorias: "Front-end",
+    Descripcion: "Social Network for Latina Women Abroad",
   },
   {
-    'id': 'Md-links',
-    'nombre': 'Md-Links.',
-    'url': 'https://www.npmjs.com/package/tatii-md-links',
-    'tecnologias': 'JavaScript API ,Node.js, Markdown ,(Git y GitHub) , HTTP , Testing con JEST',
-    'categorias': 'Back-end',
-    'imagen': 'https://github.com/Tati20h/DEV008-md-links/blob/main/img/mdlinks.png?raw=true',
-    'Descripcion': 'JavaScript CLI interface for validate files by path.',
+    id: "Md-links",
+    nombre: "Md-Links.",
+    url: "https://www.npmjs.com/package/tatii-md-links",
+    tecnologias: "JavaScript API, Node.js, Markdown, HTTP, Testing con JEST",
+    categorias: "Back-end",
+    imagen:
+      "https://github.com/Tati20h/DEV008-md-links/blob/main/img/mdlinks.png?raw=true",
+    Descripcion: "JavaScript CLI interface for validate files by path.",
   },
   {
-    'id': 'burger-queen-api-client',
-    'nombre': 'Burger Queen',
-    'url': 'https://github.com/Tati20h/DEV009-burger-queen-api-client',
-    'tecnologias': 'HTML, CSS, JS, REACT, HTTP, (Git y GitHub), FIGMA, Mock APIs, npm-scripts',
-    'categorias': 'Front-end',
-    'imagen': 'https://github.com/Tati20h/DEV009-burger-queen-api-client/blob/main/src/assets/img/burger.png?raw=true',
-    'Descripcion': 'Ecommerce Burger Restaurant Website'
+    id: "burger-queen-api-client",
+    nombre: "Burger Queen",
+    url: "https://github.com/Tati20h/DEV009-burger-queen-api-client",
+    tecnologias: "REACT.JS, FIGMA, Mock APIs, SWAGGER, NODEJS",
+    categorias: "Front-end",
+    imagen:
+      "https://github.com/Tati20h/DEV009-burger-queen-api-client/blob/main/src/assets/img/burger.png?raw=true",
+    Descripcion: "Ecommerce Burger Restaurant Website",
   },
   {
-    'id': '06-proyecto1',
-    'nombre': 'Mis Pelis.',
-    'url': 'https://github.com/Tati20h/06-proyecto1',
-    'tecnologias': 'HTML, CSS, JS, REACT, FIGMA,',
-    'categorias': 'Front-end',
-    'imagen': 'https://github.com/Tati20h/06-proyecto1/blob/main/src/img/Movies.png?raw=true',
-    'Descripcion': 'Movie streaming platform'
+    id: "06-proyecto1",
+    nombre: "Mis Pelis.",
+    url: "https://github.com/Tati20h/06-proyecto1",
+    tecnologias: "REACT.JS, FIGMA",
+    categorias: "Front-end",
+    imagen:
+      "https://github.com/Tati20h/06-proyecto1/blob/main/src/img/Movies.png?raw=true",
+    Descripcion: "Movie streaming platform",
   },
-
-]
+];
